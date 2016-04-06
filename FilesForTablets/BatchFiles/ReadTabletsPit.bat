67c51d12ba60bb2655a527bafc2725d53639d8fe
LauraRhodes <laura@pyxiseng.com>
2016-04-06 09:07:15 -0700

    batch files - upload into single directory, changed name of team photo storage location
    Batch files that read the pit and match data files from the tablets now
    put a copy of the files into single files for match and pit scouting in
    addition to making tablet specific copies.
    Changed the name of the subdirectory where team photos are stored
    (python utility ConvertPitPhotos.py.

--- FilesForTablets/BatchFiles/ReadTabletsPit.bat
+++ FilesForTablets/BatchFiles/ReadTabletsPit.bat
@@ -1,33 +1,42 @@
 set location="C:\TabletFiles\testfiles"
+set loctabletfiles="C:\TabletFiles\ingest-pit"
+
 
 adb kill-server
 adb devices
 
 echo Red1 Tablet
 adb -s 077dbf0c pull sdcard/Scouting/Pit %location%/Red1/Scouting/Pit
+adb -s 077dbf0c pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 077dbf0c pull sdcard/Pictures %location%/Red1/Pictures
 
 echo Red2 Tablet
 adb -s 08e5add0 pull sdcard/Scouting/Pit %location%/Red2/Scouting/Pit
+adb -s 08e5add0 pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 08e5add0 pull sdcard/Pictures %location%/Red2/Pictures
 
 echo Red3 Tablet
 adb -s 09e0d463 pull sdcard/Scouting/Pit %location%/Red3/Scouting/Pit
+adb -s 09e0d463 pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 09e0d463 pull sdcard/Pictures %location%/Red3/Pictures
 
 echo Blue1 Tablet
 adb -s 093291ff pull sdcard/Scouting/Pit %location%/Blue1/Scouting/Pit
+adb -s 093291ff pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 093291ff pull sdcard/Pictures %location%/Blue1/Pictures
 
 echo Blue2 Tablet
 adb -s 0a0eb95a pull sdcard/Scouting/Pit %location%/Blue2/Scouting/Pit
+adb -s 0a0eb95a pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 0a0eb95a pull sdcard/Pictures %location%/Blue2/Pictures
 
 echo Blue3 Tablet
 adb -s 09f27d4b pull sdcard/Scouting/Pit %location%/Blue3/Scouting/Pit
+adb -s 09f27d4b pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 09f27d4b pull sdcard/Pictures %location%/Blue3/Pictures
 
 echo Spare Tablet
 adb -s 0a845d90 pull sdcard/Scouting/Pit %location%/spare/Scouting/Pit
+adb -s 0a845d90 pull sdcard/Scouting/Pit %loctabletfiles%
 adb -s 0a845d90 pull sdcard/Pictures %location%/spare/Pictures
 
